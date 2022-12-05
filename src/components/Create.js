0784efff53d3255028cfb42b69f7ffec40b5f69c
aaronjan98 <aaronjan98@gmail.com>
2022-12-05 10:51:52 -0800

    create the form onSubmit handler for creating new proposals

--- src/components/Create.js
+++ src/components/Create.js
@@ -5,19 +5,48 @@ import Spinner from 'react-bootstrap/Spinner'
 import { ethers } from 'ethers'
 
 const Create = ({ provider, dao, setIsLoading }) => {
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState(0)
+  const [address, setAddress] = useState('')
+
+  const createHandler = async e => {
+    e.preventDefault()
+
+    try {
+      const signer = await provider.getSigner()
+      const formattedAmount = await ethers.utils.parseEther(amount.toString())
+
+      const transaction = await dao
+        .connect(signer)
+        .createProposal(name, formattedAmount, address)
+      await transaction.wait()
+    } catch {
+      window.alert('User rejected or transaction reverted')
+    }
+
+    setIsLoading(true)
+  }
+
   return (
-    <Form>
+    <Form onSubmit={createHandler}>
       <Form.Group style={{ maxWidth: '450px', margin: '50px auto' }}>
-        <Form.Control type="text" placeholder="Enter name" className="my-2" />
+        <Form.Control
+          type="text"
+          placeholder="Enter name"
+          className="my-2"
+          onChange={e => setName(e.target.value)}
+        />
         <Form.Control
           type="number"
           placeholder="Enter amount"
           className="my-2"
+          onChange={e => setAmount(e.target.value)}
         />
         <Form.Control
           type="text"
           placeholder="Enter address"
           className="my-2"
+          onChange={e => setAddress(e.target.value)}
         />
         <Button variant="primary" type="submit" style={{ width: '100%' }}>
           Create Proposal
