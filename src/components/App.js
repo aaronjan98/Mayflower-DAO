02c52cc2e700367b931e633d1835afe01673352a
aaronjan98 <aaronjan98@gmail.com>
2022-12-04 16:34:22 -0800

    initiate contracts and render treasury balance in src/components/App.js

--- src/components/App.js
+++ src/components/App.js
@@ -7,20 +7,38 @@ import Navigation from './Navigation'
 import Loading from './Loading'
 
 // ABIs: Import your contract ABIs here
-// import TOKEN_ABI from '../abis/Token.json'
+import DAO_ABI from '../abis/DAO.json'
 
 // Config: Import your network config here
-// import config from '../config.json';
+import config from '../config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null)
   const [account, setAccount] = useState(null)
-  const [balance, setBalance] = useState(0)
+  const [dao, setDao] = useState(null)
+  const [treasuryBalance, setTreasuryBalance] = useState(0)
 
   const [isLoading, setIsLoading] = useState(true)
 
   const loadBlockchainData = async () => {
     // Initiate provider
     const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const { chainId } = await provider.getNetwork()
+
+    // Initiate contracts
+    const dao = new ethers.Contract(
+      config[chainId].dao.address,
+      DAO_ABI,
+      provider
+    )
+    setDao(dao)
+
+    // Fetch treasury balance
+    let treasuryBalance = await provider.getBalance(dao.address)
+    treasuryBalance = ethers.utils.formatUnits(treasuryBalance, 18)
+    setTreasuryBalance(treasuryBalance)
 
     // Fetch accounts
     const accounts = await window.ethereum.request({
@@ -28,11 +46,6 @@ function App() {
     })
     const account = ethers.utils.getAddress(accounts[0])
     setAccount(account)
-
-    // Fetch account balance
-    let balance = await provider.getBalance(account)
-    balance = ethers.utils.formatUnits(balance, 18)
-    setBalance(balance)
 
     setIsLoading(false)
   }
@@ -53,10 +66,12 @@ function App() {
         <Loading />
       ) : (
         <>
+          <hr />
           <p className="text-center">
-            <strong>Your ETH Balance:</strong> {balance} ETH
+            <strong>Treasury Balance: </strong>
+            {treasuryBalance} ETH
           </p>
-          <p className="text-center">Edit App.js to add your code here.</p>
+          <hr />
         </>
       )}
     </Container>
