707a09c8c0ecd540f636f76564d72e8308829298
aaronjan98 <aaronjan98@gmail.com>
2022-12-30 23:51:54 -0800

    update script to log out all user token balances

--- scripts/getBalances.js
+++ scripts/getBalances.js
@@ -1,12 +1,15 @@
 const config = require('../src/config.json')
 
 async function main() {
-  let accounts, deployer, minter
+  let accounts, deployer, minter, investor2, investor3, recipient
 
   // Create the accounts
   accounts = await ethers.getSigners()
   deployer = accounts[0]
   minter = accounts[1]
+  investor2 = accounts[2]
+  investor3 = accounts[3]
+  recipient = accounts[4]
 
   // print all users balances
   for (let i = 0; i < accounts.length; i++) {
@@ -22,9 +25,25 @@ async function main() {
   // Fetch network
   const { chainId } = await ethers.provider.getNetwork()
 
-  // Fetch deployed token
+  // Fetch deployed contracts
+  const token = await ethers.getContractAt(
+    'Token',
+    config[chainId].token.address
+  )
   const dao = await ethers.getContractAt('DAO', config[chainId].dao.address)
-  // console.log(`DAO fetched: ${dao.address}\n`)
+
+  console.log('♥ ´¨`•.¸¸.♫│▌▌▌│▌▌│▌▌▌│▌▌│▌▌▌♫´¨`*•.¸¸♥')
+
+  // print all users' token balances
+  for (let i = 0; i < accounts.length; i++) {
+    account = accounts[i].address
+    const tokenBalance = await token.balanceOf(ethers.utils.getAddress(account))
+    console.log(
+      `${i}: ${account} ${await ethers.utils.formatEther(
+        tokenBalance.toString()
+      )}`
+    )
+  }
 }
 
 main().catch(error => {
